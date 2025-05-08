2240230e6fcce6f1021b15d0cb632a9257d3e741
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2025-05-08 05:39:28 -0400

    Add rotating arrow indicator

--- src/drop-down.js
+++ src/drop-down.js
@@ -6,11 +6,18 @@ function createDropdown(id, callback, placeholder, data) {
     // Create the outer container
     const container = document.createElement("div");
     container.id = id;
+    container.classList = "dropdown";
 
     // Create the input box
     const input = document.createElement("div");
     input.classList = "dropdown-input dropdown-placeholder";
     input.textContent = placeholder;
+
+    // Create an arrow indicator.
+    // We are using a triangle character for this
+    const arrow = document.createElement("div");
+    arrow.classList = "dropdown-arrow";
+    arrow.innerText = "\u25BC";
 
     // Create a panel to house the data items
     const panel = document.createElement("div");
@@ -23,12 +30,26 @@ function createDropdown(id, callback, placeholder, data) {
         const allPanels = document.querySelectorAll(".dropdown-panel");
         allPanels.forEach(somePanel => {
             if (panel !== somePanel) {
+                // Hide the panel
                 somePanel.classList.add("hide");
+
+                // Un-rotate the arrow
+                const parentDropDown = somePanel.parentNode;
+                const someArrow = parentDropDown.querySelector(".dropdown-arrow");
+                someArrow.style.transform = "rotate(0deg)";
             }
         });
 
-        // Toggle the visibility of this panel
-        panel.classList.toggle("hide");
+        // Toggle the visibility of this panel and rotate
+        // the arrow indicator
+        if (panel.classList.contains("hide")) {
+            arrow.style.transform = "rotate(180deg)";
+            panel.classList.remove("hide");
+        }
+        else {
+            arrow.style.transform = "rotate(0deg)";
+            panel.classList.add("hide");
+        }
     })
 
     // Iterate through the data array and add each item to
@@ -51,12 +72,16 @@ function createDropdown(id, callback, placeholder, data) {
 
             // Hide the panel
             panel.classList.toggle("hide");
+
+            // Un-rotate the arrow
+            arrow.style.transform = "rotate(0deg)";
         });
         panel.appendChild(optionItem);
     });
 
     // Add the elements to the container
     container.appendChild(input);
+    container.appendChild(arrow);
     container.appendChild(panel);
 
     return container;
